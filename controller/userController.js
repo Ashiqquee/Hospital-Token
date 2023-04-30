605134fbf7eac62a2ea01e09e71c50a46bd5b3c2
Ashiqquee <ashiqqueee@gmail.com>
2023-04-30 15:15:04 +0530

    login completed

--- controller/userController.js
+++ controller/userController.js
@@ -1,4 +1,15 @@
 const User = require('../model/userModel');
+const bcrypt = require('bcrypt')
+
+const securePassword = async (password) => {
+    try {
+        const passwordHash = await bcrypt.hash(password, 10);
+        return passwordHash;
+    } catch (error) {
+        console.error(error);
+        throw new Error("Failed to hash password");
+    }
+};
 
 module.exports = {
 
@@ -24,6 +35,7 @@ module.exports = {
             const { username, password, phone } = req.body;
             const userName = username.toLowerCase();
             const existingUserName = await User.findOne({ userName: userName });
+            const bcryptedPassword = await securePassword(password);
             if (existingUserName) {
                 res.json({ status: false, err: "username" });
             } else {
@@ -34,16 +46,46 @@ module.exports = {
                     const newUser = new User({
                         userName: userName,
                         phone:phone,
-                        password:password
+                        password: bcryptedPassword,
                     })
                     await newUser.save();
                     res.json({ status: true })
                 }
             }
         } catch (error) {
-            console.log(error + "deeeeeeeeeeee");
+            console.log(error);
         }
-    }
+    }, 
+
+    verifyLogin : async(req,res) => {
+        try {
+            const {username,password} = req.body;
+            const userName = username.toLowerCase();
+            const existingUser = await User.findOne({userName:userName});
+            if(existingUser){
+                const passwordMatch = await bcrypt.compare(password, existingUser.password);
+                if(passwordMatch){
+                    req.session.userId = existingUser._id;
+                    console.log(req.session.userId);
+                    res.json({status:true});
+                } else {
+                    res.json({status:false,err:"password"});
+                }
+            } else {
+                res.json({status:false,err:"user"})
+            }
+        } catch (error) {
+           console.log(error); 
+        }
+    },
+
+    loadHome : async (req,res) => {
+        try {
+           res.render('../views/user/home.ejs') 
+        } catch (error) {
+            console.log(error);
+        }
+    },
 
 
 }
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -4,6 +4,7 @@ const userRoute =  express();
 const Usercontroller = require('../controller/userController');
 const authentication = require('../middleware/userAuth');
 const checkBlocked = require('../middleware/userBlocked');
+const userController = require('../controller/userController');
 const isLogin = authentication.isLogout;
 const isLogout = authentication.isLogin;
 const isBlocked = checkBlocked.isBlocked;
@@ -11,11 +12,15 @@ const isBlocked = checkBlocked.isBlocked;
 
 userRoute.get('/login',isLogin,Usercontroller.loadLogin);
 
-userRoute.post('/login', isLogin, Usercontroller.loadLogin);
+userRoute.post('/login', isLogin, Usercontroller.verifyLogin);
 
 userRoute.get('/signup',isLogin,Usercontroller.loadSignup);
 
 userRoute.post('/register', isLogin, Usercontroller.addUser);
+
+userRoute.get('/', isBlocked,userController.loadHome);
+
+
 
 
 
